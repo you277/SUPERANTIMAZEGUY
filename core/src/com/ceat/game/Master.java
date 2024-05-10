b50bf786ebbf8e40a5b296120401ab20f3a6322e
you277 <yuxuanl7@nycstudents.net>
2024-05-10 15:34:44 -0400

    asdfgsdfgdsfg

--- core/src/com/ceat/game/Master.java
+++ core/src/com/ceat/game/Master.java
@@ -2,6 +2,7 @@ package com.ceat.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -30,7 +31,7 @@ public class Master extends ApplicationAdapter implements InputProcessor {
 		sprite.setSize(100, 100);
 		cam = new OrthographicCamera(800, 600);
 		cam2 = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		model = new ModelBuilder().createBox(5, 0, 5,
+		model = new ModelBuilder().createBox(5, 5, 5,
 				new Material( ColorAttribute.createDiffuse(Color.WHITE) ), VertexAttributes.Usage.Position);
 		instance = new SimpleModelInstance(model);
 	}
@@ -42,6 +43,12 @@ public class Master extends ApplicationAdapter implements InputProcessor {
 	public float getScaledDeltaTime() {
 		return getDeltaTime()*gameSpeed;
 	}
+	float horizontalRot;
+	float verticalRot;
+	float x;
+	float y;
+	float z = 10;
+	float camDist = 10;
 
 	@Override
 	public void render() {
@@ -55,8 +62,51 @@ public class Master extends ApplicationAdapter implements InputProcessor {
 //		cam.update();
 //		batch.setProjectionMatrix(cam.combined);
 
-		cam2.position.set(10, 10, 10);
-		cam2.lookAt(0, 0, 0);
+		if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+			z -= delta*30;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+			z += delta*30;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+			x -= delta*30;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+			x += delta*30;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.E)) {
+			y += delta*30;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
+			y -= delta*30;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			horizontalRot = (horizontalRot - delta*180)%360;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			horizontalRot = (horizontalRot + delta*180)%360;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			verticalRot = Math.min(verticalRot + delta*180, 80);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			verticalRot = Math.min(verticalRot - delta*180, 80);
+		}
+
+		double polar = Math.toRadians(horizontalRot);
+		double alpha = Math.toRadians(verticalRot);
+
+		float xOff = camDist * (float)(Math.cos(polar) * Math.cos(alpha));
+		float yOff = camDist * (float)(Math.cos(polar) * Math.sin(alpha));
+		float zOff = camDist * (float)Math.sin(polar);
+
+		float finalX = x + xOff;
+		float finalY = y + yOff;
+		float finalZ = y + zOff;
+
+		cam2.position.set(x, y, z);
+//		cam2.;
+		cam2.lookAt(finalX, finalY, finalZ);
 		cam.near = 1;
 		cam.far = 300;
 		cam2.update();
@@ -65,7 +115,7 @@ public class Master extends ApplicationAdapter implements InputProcessor {
 
 
 //instance.transform.set(new Vector3(0, (float)Math.sin(elapsed)*5, 0), new Quaternion());
-		instance.setPosition(0, (float)Math.sin(elapsed)*5, 0).setRotation(0, elapsed*360, 0);
+		instance.setPosition(0, (float)Math.sin(elapsed)*5, 0).setRotation(elapsed*90, elapsed*90, elapsed*90);
 		batch2.begin(cam2);
 		instance.render(batch2);
 //		batch2.render(instance);
